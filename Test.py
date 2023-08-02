2ef612ad8e2c8433fcba697178f57dd5397e69d4
agrawal-admin <agrawal-admin@SON-SA-448340.cntrlsrvs.w2k.vt.edu>
2023-08-02 12:50:08 -0400

    UpdateRepo

---
+++ Test.py
@@ -0,0 +1,30 @@
+from Phidget22.Phidget import *
+from Phidget22.Devices.DCMotor import *
+from Phidget22.Devices.Encoder import *
+import time
+
+def onPositionChange(self, positionChange, timeChange, indexTriggered):
+	print("PositionChange: " + str(positionChange))
+	print("TimeChange: " + str(timeChange))
+	print("IndexTriggered: " + str(indexTriggered))
+	print("getPosition: " + str(self.getPosition()))
+	print("----------")
+def main():
+	dcMotor0 = DCMotor()
+	encoder0 = Encoder()
+
+	encoder0.setOnPositionChangeHandler(onPositionChange)
+	dcMotor0.openWaitForAttachment(5000)
+	encoder0.openWaitForAttachment(5000)
+
+	dcMotor0.setTargetVelocity(-0.2)
+
+	try:
+		input("Press Enter to Stop\n")
+	except (Exception, KeyboardInterrupt):
+		pass
+
+	dcMotor0.close()
+	encoder0.close()
+
+main()
